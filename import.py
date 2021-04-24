534a588542aab65049ee58bad25a95eea7f9f15d
Biruk-Tesfaye-Legesse-IT <it.biruk.tesfaye2913@gmail.com>
2021-04-24 23:33:35 +0300

    Final III

--- import.py
+++ import.py
@@ -3,7 +3,7 @@ import csv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session,sessionmaker
 
-link = "postgresql://postgres:root@localhost/book_store" 
+link = "postgresql://cqrlmoqswiquqf:da8d2dcbe9d00c77ad09320c8eb86851b0243599c5b62fa8c280bbedf582b96f@ec2-52-87-107-83.compute-1.amazonaws.com:5432/d4n1i8iase18lm" 
 
 engine = create_engine(link)
 db = scoped_session(sessionmaker(bind=engine))
--- inst__tables/books_table.py
+++ inst__tables/books_table.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from inst__tables.books_model import *
+from books_model import *
 link = ""
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:root@localhost/book_store"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://cqrlmoqswiquqf:da8d2dcbe9d00c77ad09320c8eb86851b0243599c5b62fa8c280bbedf582b96f@ec2-52-87-107-83.compute-1.amazonaws.com:5432/d4n1i8iase18lm"
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
 db.init_app(app)
 
